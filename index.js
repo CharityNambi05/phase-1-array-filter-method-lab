3fded95a5de6daaa3459ae907da19b526da405f8
CharityNambi05 <charity.nambi@icloud.com>
2024-07-13 19:59:25 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+// Code your solution hereconst drivers 
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+
+  function fuzzyMatch(drivers, partial) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(partial.toLowerCase()));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name == name);
+  }
+   
+
+
